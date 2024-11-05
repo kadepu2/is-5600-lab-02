81e0170a906feed40025f6454b02e1d92b9288a0
kadepu2 <kadepu2@slu.edu>
2024-11-05 17:58:52 +0000

    submission

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
+    const deleteButton = document.querySelector("#btnDelete");
+    const saveButton = document.querySelector("#btnSave");
+    generateUserList(userData, stocksData);
+  
+  
+  deleteButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const userId = document.querySelector('#userID').value;
+  const userIndex = userData.findIndex(user => user.id == userId);
+  userData.splice(userIndex, 1);
+  generateUserList(userData, stocksData);
+  });
+  
+  
+  
